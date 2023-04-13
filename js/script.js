ac1d8a1cc9e112782bc22076a43c21baabf041c3
Adlet <example@gmail.com>
2023-04-14 02:32:21 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+// alert('Hello');
+
+// const result = confirm("Are you here ?");
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18?","");
+// console.log(answer +5);
+
+const answer= [];
+answer[0] = prompt("Ваше имя ?", ""),
+answer[1] = prompt("Сколько вам лет?", "")
+answer[2] = prompt("Ваше имя ?", "");
+
+document.write(answer);
